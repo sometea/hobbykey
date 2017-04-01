49a95007c9b13182fcf8ae73df9e1dd1f28eb008
David Dasenbrook <david.dasenbrook@gmail.com>
2017-04-01 09:52:29 +0200

    add an angular service for the releases and make PostsService and ReleasesService extend a common base service

---
+++ client/common/get.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GetService {
+  protected getUrl = '/api/posts';
+
+  constructor(private http: Http) {
+  }
+
+  getPosts() {
+    return this.http.get(this.getUrl)
+      .map(response => response.json())
+      .catch(err => this.handleError(err));
+  }
+
+  handleError(error) {
+    if (error.status === 401) console.log('Access denied');
+    console.log('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
--- client/releases/releases.service.ts
+++ client/releases/releases.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
-import { GetService } from '../common/get.service';
+import { GetService } from "../common/get.service";
 
 @Injectable()
 export class PostsService extends GetService {
-  protected getUrl = '/api/posts';
+  protected getUrl = '/api/releases';
 
   constructor(http: Http) {
     super(http);
