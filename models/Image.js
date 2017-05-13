123743725ecd43956bc4c7ec96ddfb6c8bb49e92
David Dasenbrook <david.dasenbrook@tngtech.com>
2017-05-13 21:49:40 +0200

    add some initial file type validation experiment

--- models/Image.js
+++ models/Image.js
@@ -10,5 +10,13 @@ Image.add({
   file: { type: Types.File, storage: uploadedFilesStorage },
 });
 
+Image.schema.pre('validate', function(next) {
+  if (this.file.originalname !== 'test.png' && this.file.originalname) {
+    let err = new Error('Not the correct file');
+    next(err);
+  }
+  next();
+});
+
 Image.defaultColumns = 'title, file, url';
 Image.register();
