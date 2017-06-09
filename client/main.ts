5c76d41e2a1b9a92d3c850f8990c78b02f85dc6a
David Dasenbrook <david.dasenbrook@tngtech.com>
2017-06-09 21:02:23 +0200

    move prod mode down

--- client/main.ts
+++ client/main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+platformBrowserDynamic().bootstrapModule(AppModule);
+
 // Enable production mode unless running locally
 if (!/localhost/.test(document.location.host)) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
